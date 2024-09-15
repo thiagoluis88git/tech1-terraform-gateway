350481b1b9a4fd70aaf3de5f8a2076a18ba780ea
Thiago Luis <thiago.luis88@gmail.com>
2024-09-14 23:00:10 -0300

    feat: add verify_access_token with Cognito function

--- src/lambda_authorizer.py
+++ src/lambda_authorizer.py
@@ -1,4 +1,6 @@
 import json
+import boto3
+from botocore.exceptions import ClientError
 
 def generate_policy(principal_id, effect, resource):
     auth_response = {
@@ -20,10 +22,38 @@ def generate_policy(principal_id, effect, resource):
 
     return auth_response
 
+def verify_access_token(access_token):
+    # Initialize a boto3 client for Cognito
+    client = boto3.client('cognito-idp')
+
+    try:
+        # Call GetUser API with the provided access token
+        response = client.get_user(
+            AccessToken=access_token
+        )
+        
+        # If the access token is valid, you will get user information
+        print("Access token is valid!")
+        print("User attributes:", response['UserAttributes'])
+        
+        return True
+    
+    except ClientError as e:
+        # Handle exceptions (such as invalid token)
+        if e.response['Error']['Code'] == 'NotAuthorizedException':
+            print("Access token is invalid.")
+        else:
+            print(f"An error occurred: {e}")
+        
+        return False
 
 def lambda_handler(event, context):
     token = event['authorizationToken']
 
+    isValid = verify_access_token(token)
+
+    print("É valido?", isValid)
+    
     valid_token = 'xyz987'
 
     if token == valid_token:
